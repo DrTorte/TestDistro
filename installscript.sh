0d2b68223c7cec5e15ff8a5d050c01ea6fa41b0e
Kristofer <github@drtorte.net>
2018-08-27 23:56:23 -0400

    hi!

--- installscript.sh
+++ installscript.sh
@@ -1,4 +1,2 @@
-curl -s -o /dev/null -w ".org: %{http_code} \n \n" 
-https://api.travis-ci.org/jobs
-curl -s -o /dev/null -w ".com: %{http_code}" 
-https://api.travis-ci.com/jobs
+curl -s -o /dev/null -w ".org: %{http_code}\n\n" https://api.travis-ci.org/jobs
+curl -s -o /dev/null -w ".com: %{http_code}" https://api.travis-ci.com/jobs
